c6386081dba38cba22c2ac892565246b089af179
codal-tshah <tshah@codal.com>
2024-04-22 11:06:56 +0530

    CIT-289 #time 30m Updated the .py file for PySpark DataFrame

--- PySpark/pyspark_dataframe.py
+++ PySpark/pyspark_dataframe.py
@@ -292,3 +292,59 @@ df.printSchema()
 
 # getItem() used with MapType
 df.select(df.lname.getItem("xxx")).show()
+
+
+"""# **Select Columns**"""
+
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("Spark.examples.com").getOrCreate()
+data = [
+    ("mahesh ihfweoifow", "sci", 15, "ssoa"),
+    ("ramesh", "comm", 17, "xaviers"),
+    ("fenil a;h;hwo;eihfwoeih", "arts", 16, "dhl"),
+    ("kenil", "sci", 18, "infocity"),
+]
+columns = ["Name", "stream", "Age", "School"]
+df = spark.createDataFrame(data=data, schema=columns)
+df.show()
+df.printSchema()
+
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("SparkByExamples.com").getOrCreate()
+data = [
+    ("James", "Smith", "USA", "CA"),
+    ("Michael", "Rose", "USA", "NY"),
+    ("Robert", "Williams", "USA", "CA"),
+    ("Maria", "Jones", "USA", "FL"),
+]
+columns = ["firstname", "lastname", "country", "state"]
+df = spark.createDataFrame(data=data, schema=columns)
+df.show(truncate=False)
+
+df.select("firstname", "lastname").show()
+df.select(df.firstname, df.lastname).show()
+df.select(df["firstname"], df["lastname"]).show()
+
+# By using col() function
+from pyspark.sql.functions import col
+
+df.select(col("firstname"), col("lastname")).show()
+
+# Select columns by regular expression
+df.select(df.colRegex("`^.*name*`")).show()
+
+df.select(*columns).show()
+
+# Select All columns
+df.select([col for col in df.columns]).show()
+df.select("*").show()
+
+# Selects first 3 columns and top 3 rows
+df.select(df.columns[:3]).show(3)
+
+# Selects columns 2 to 4  and top 3 r
+df.select(df.columns[2:4]).show(3)
